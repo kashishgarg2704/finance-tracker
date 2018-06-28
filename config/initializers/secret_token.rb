cf1512889213c1a5bc385d42e6577d515d9e5ef5
kashish.garg <kashishgarg27@gmail.com>
2018-06-28 12:23:27 +0530

    finance-tracker

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinanceTracker::Application.config.secret_token = '4ed7803bb744015534f87fbfaa68ec4b577495253860f4c20852ffdb7a5bd541a5ce6f244d67a217af9fb7bfd424453d7e2e34b4237ed0e74f8b7ce4244b5a90'
